ce308af48317a24f04a962acfdae1e926ff8fd68
MathisCorbeil <catmath23@gmail.com>
2025-04-09 15:51:46 -0400

    pa

--- Assets/Scripts/MiniJeu.cs
+++ Assets/Scripts/MiniJeu.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MiniJeu : MonoBehaviour
 {
     public float pointageTemps;
+    [SerializeField] TextMeshProUGUI _Score;
 
     void Start()
     {
         pointageTemps = 0;
+    }
+
+    private void Update()
+    {
+        _Score.text = pointageTemps.ToString("00.00");
     }
 }
